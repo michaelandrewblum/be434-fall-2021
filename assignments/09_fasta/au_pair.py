679cd32a7fb5ec81a8c162a1b942dfc9ee5ba42c
Michael Blum <michaelblum@email.arizona.edu>
2021-11-02 10:59:24 -0700

    working version

--- assignments/09_fasta/au_pair.py
+++ assignments/09_fasta/au_pair.py
@@ -7,8 +7,6 @@ Purpose: Split interleaved/paired reads
 
 import argparse
 import os
-import errno
-from pathlib import Path
 from Bio import SeqIO
 
 
@@ -41,10 +39,6 @@ def main():
 
     args = get_args()
 
-    # print(args)
-    # print(os.path.isdir(args.outdir))
-    # print(f'>>> "{args.outdir}"')
-
     if not os.path.isdir(args.outdir):
         os.makedirs(args.outdir)
 
@@ -55,41 +49,69 @@ def main():
         input_files.append(file)
         file_names.append(os.path.splitext(file.name))
 
-    # print(input_files)
-    # print(file_names)
-
     for file in input_files:
         basename = os.path.basename(file.name)
         root, ext = os.path.splitext(basename)
 
         fwd_out = os.path.join(args.outdir, root + '_1' + ext)
         rev_out = os.path.join(args.outdir, root + '_2' + ext)
-        
-        reader = list(SeqIO.parse(file.name, 'fasta'))
 
-        fwd_seq_list = []
-        rev_seq_list = []
+        fwd_seq_list, rev_seq_list = interleaved_fasta(file)
 
-        for num, rec in enumerate(reader):
-            if num % 2 == 0:
-                fwd_seq_list.append(('>' + rec.description, str(rec.seq)))
-            else:
-                rev_seq_list.append(('>' + rec.description, str(rec.seq)))
-
-        # print(len(fwd_seq_list), len(rev_seq_list), len(reader))
-
-        with open(fwd_out, 'wt') as fh_fwd:
-            for seq in fwd_seq_list:
-                fh_fwd.write(seq[0] + '\n')
-                fh_fwd.write(seq[1] + '\n')
-
-        with open(rev_out, 'wt') as fh_rev:
-            for seq in rev_seq_list:
-                fh_rev.write(seq[0] + '\n')
-                fh_rev.write(seq[1] + '\n')
+        write_fasta(fwd_seq_list, fwd_out)
+        write_fasta(rev_seq_list, rev_out)
 
     print(f'Done, see output in "{args.outdir}"')
-        
+
+
+# --------------------------------------------------
+def write_fasta(sequences, outfile):
+    """ Write fasta lines to file """
+
+    with open(outfile, 'wt', encoding='utf-8') as fh:
+        for seq in sequences:
+            fh.write(seq[0] + '\n')
+            fh.write(seq[1] + '\n')
+
+
+# --------------------------------------------------
+def interleaved_fasta(fh):
+    """ Create sequences for interleaved fasta data """
+
+    reader = list(SeqIO.parse(fh.name, 'fasta'))
+    fwd_seq_list = []
+    rev_seq_list = []
+
+    for num, rec in enumerate(reader):
+        if num % 2 == 0:
+            fwd_seq_list.append(('>' + rec.description, str(rec.seq)))
+        else:
+            rev_seq_list.append(('>' + rec.description, str(rec.seq)))
+
+    return fwd_seq_list, rev_seq_list
+
+
+# --------------------------------------------------
+def test_interleaved_fasta():
+    """ Test iterleaved_fasta function """
+
+    with open('./inputs/reads1.fa', 'rt', encoding='utf-8') as fh:
+
+        fwd, rev = interleaved_fasta(fh)
+
+        assert fwd == [
+            ('>M10991:61:000000000-A7EML:1:1101:14011:1001 1:N:0:28',
+             'NGCTCCTAGGTCGGCATGATGGGGGAAGGAGAGCATGGGAAGAAATGAGAGAGTAGCAA'),
+            ('>M10991:61:000000000-A7EML:1:1201:15411:3101 1:N:0:28',
+             'NGCTCCTAGGTCGGCATGATGGGGGAAGGAGAGCATGGGAAGAAATGAGAGAGTAGCAA')
+        ]
+        assert rev == [
+            ('>M10991:61:000000000-A7EML:1:1101:14011:1001 2:N:0:28',
+             'NGCTCCTAGGTCGGCATGACGCTAGCTACGATCGACTACGCTAGCATCGAGAGTAGCAA'),
+            ('>M10991:61:000000000-A7EML:1:1201:15411:3101 2:N:0:28',
+             'CGCTAGCTACGACTCGACGACAGCGAACACGCGATCGATCGGAAATGAGAGAGTAGCAA')
+        ]
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
